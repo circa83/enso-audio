2e81078b3034fc5e535bedb125323d6d761e2380
circa83 <hello@enso-audio.com>
2025-05-08 06:42:43 -0700

    uptodate

--- src/lib/data/tracks.ts
+++ src/lib/data/tracks.ts
@@ -5,36 +5,36 @@ export const musicLibrary: Track[] = [
   {
     id: '1',
     title: 'Stillness',
-    artist: 'Ensō Audio',
+    artist: 'Brock Cooper',
     src: '/audio/February2(short)_2025.mp3',
-    artwork: '/images/Stillness_EnsōAudio_bkcp.png'
+    artwork: '/images/Stillness_EnsoAudio_bkcp.png'
   },
   {
     id: '2',
     title: 'Breathe',
-    artist: 'Ensō Audio',
+    artist: 'Brock Cooper',
     src: '/audio/March4(short)_2025.mp3',
-    artwork: '/images/Breathe_EnsōAudio_bkcp.png'
+    artwork: '/images/Breathe_EnsoAudio_bkcp.png'
   },
   {
     id: '3',
     title: 'Eclipse',
-    artist: 'Ensō Audio',
+    artist: 'Brock Cooper',
     src: '/audio/March5(short)_2025.mp3',
-    artwork: '/images/Eclipse_EnsōAudio_bkcp.png'
+    artwork: '/images/Eclipse_EnsoAudio_bkcp.png'
   },
   {
     id: '4',
     title: 'Empower',
-    artist: 'Ensō Audio',
+    artist: 'Brock Cooper',
     src: '/audio/March9(short)_2025.mp3',
-    artwork: '/images/Empower_EnsōAudio_bkcp.png'
+    artwork: '/images/Empower_EnsoAudio_bkcp.png'
   },
   {
     id: '5',
     title: 'Elevate',
-    artist: 'Ensō Audio',
+    artist: 'Brock Cooper',
     src: '/audio/March9(short)a_2025.mp3',
-    artwork: '/images/Elevate_EnsōAudio_bkcp.png'
+    artwork: '/images/Elevate_EnsoAudio_bkcp.png'
   }
 ];
