c717edb42d4df2da27dc4a9f2df2095458d7f228
circa83 <hello@enso-audio.com>
2025-05-08 08:25:14 -0700

    vercel building

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // Use 'nodejs20.x' which is a valid Vercel runtime
+      runtime: 'nodejs20.x'
+    })
   }
 };
 
