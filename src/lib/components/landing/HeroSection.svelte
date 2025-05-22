98308a89e952b1806cac80fb90526ccc16465969
circa83 <hello@enso-audio.com>
2025-05-21 18:19:23 -0700

    updateing

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -13,7 +13,7 @@
           class="h-16 mx-auto mb-1"
         />
         <p class="text-enso-text-secondary tracking-[2px] uppercase text-sm md:text-base">
-         Sound Design for Transformation
+         Audio + Visual + Mindfulness
         </p>
         <div class="pt-4">
           <a 
